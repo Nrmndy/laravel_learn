1dff2ba5c008f5fd005dd58d7484cef06ba4a22b
Andrew Karpov <aggressivity@gmail.com>
2022-09-06 21:08:39 +0300

    some useless changes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,16 @@
 <?php
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\FeedbackController;
 use App\Http\Controllers\StaticPageController;
@@ -10,8 +21,6 @@ Route::get('/', [ArticleController::class, 'index']);
 
 Route::get('/about', [StaticPageController::class, 'about']);
 Route::get('/contacts', [StaticPageController::class, 'contacts']);
-
-//Route::resource('/articles/', 'ArticleController');
 
 Route::get('/articles/create', [ArticleController::class, 'create']);
 Route::get('/articles/{slug}', [ArticleController::class, 'show']);
@@ -24,3 +33,9 @@ Route::get('/articles/tags/{tag}', [TagController::class, 'index']);
 
 Route::get('/admin/feedback', [FeedbackController::class, 'index']);
 Route::post('/feedback', [FeedbackController::class, 'store']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
