050994f0fb4c2c7d06dc56efc2505e00b6f52602
Andrew Karpov <aggressivity@gmail.com>
2022-10-26 21:34:15 +0300

    telescope link added

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -2,7 +2,7 @@
     <header class="blog-header py-3">
         <div class="row flex-nowrap justify-content-between align-items-center">
             <div class="col-4 pt-1">
-                <a class="text-muted" href="#">Subscribe</a>
+                <a class="text-muted" href="/telescope">Telescope</a>
             </div>
             <div class="col-4 text-center">
                 <h1><a class="blog-header-logo text-dark text-decoration-none fw-light" href="/">Хранилище статей</a></h1>
